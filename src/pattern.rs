9ecc22544d108bdf4cb5ff1aecb64adc18e5d888
WolverinDEV <git@teaspeak.de>
2024-03-12 03:27:47 +0100

    Adding support for a "Signature" type

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,4 +1,8 @@
-use alloc::vec::Vec;
+use alloc::{
+    boxed::Box,
+    string::String,
+    vec::Vec,
+};
 
 pub trait SearchPattern {
     fn length(&self) -> usize;
@@ -82,5 +86,69 @@ impl SearchPattern for ByteSequencePattern {
             .zip(target.iter())
             .find(|(pattern, value)| !pattern.matches_byte(**value))
             .is_none()
+    }
+}
+
+pub enum SignatureType {
+    /// The value is an address relative to the current instruction.
+    /// When resolved the absolute address the instruction pointed towards will be returned.
+    RelativeAddress { inst_length: usize },
+
+    /// The value is an offset within a struct.
+    /// (Offsets are assumed to be u32)
+    Offset,
+
+    /// Resolve the absolute address of the beginning of the target pattern
+    Pattern,
+}
+
+/// A signature which leads to an offset or address
+/// based on a sequence of instructions.
+pub struct Signature {
+    pub debug_name: String,
+    pub pattern: Box<dyn SearchPattern>,
+    pub offset: u64,
+    pub value_type: SignatureType,
+}
+
+impl Signature {
+    /// Create a new relative address signature from a byte sequence pattern.
+    /// Note: If the pattern is invalid this will panic!
+    pub fn relative_address(
+        debug_name: impl Into<String>,
+        pattern: &str,
+        offset: u64,
+        inst_length: usize,
+    ) -> Self {
+        let pattern = Box::new(ByteSequencePattern::parse(pattern).expect("to be a valid pattern"));
+
+        Self {
+            debug_name: debug_name.into(),
+            pattern,
+            offset,
+            value_type: SignatureType::RelativeAddress { inst_length },
+        }
+    }
+
+    pub fn offset(debug_name: impl Into<String>, pattern: &str, offset: u64) -> Self {
+        let pattern = Box::new(ByteSequencePattern::parse(pattern).expect("to be a valid pattern"));
+
+        Self {
+            debug_name: debug_name.into(),
+            pattern,
+            offset,
+            value_type: SignatureType::Offset,
+        }
+    }
+
+    pub fn pattern(debug_name: impl Into<String>, pattern: &str) -> Self {
+        let pattern = Box::new(ByteSequencePattern::parse(pattern).expect("to be a valid pattern"));
+
+        Self {
+            debug_name: debug_name.into(),
+            pattern,
+            offset: 0x00,
+            value_type: SignatureType::Pattern,
+        }
     }
 }
